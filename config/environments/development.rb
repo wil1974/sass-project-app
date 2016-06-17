aa3c31207a25b82e4c7d0053ea41eeecddccd3c8
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-06-17 15:01:19 +0000

    added sendgrid config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #sass-app sendgrid addon config
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://sass-project-app.herokuapp.com/'}
 end
